db05237edc202dd9f1d469d0dcc12cd654d55902
denys <mystalowawola@gmail.com>
2017-03-13 15:14:39 +0200

    Init

---
+++ start/login.h
@@ -0,0 +1,28 @@
+#ifndef LOGIN_H
+#define LOGIN_H
+
+#include <QMainWindow>
+#include <QtSql>
+#include <QtDebug>
+
+namespace Ui {
+class Login;
+}
+
+class Login : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit Login(QWidget *parent = 0);
+    ~Login();
+
+private slots:
+    void on_pushButton_clicked();
+
+private:
+    Ui::Login *ui;
+    QSqlDatabase db_driver;
+};
+
+#endif // LOGIN_H
---
+++ start/start.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2017-03-13T13:43:24
+#
+#-------------------------------------------------
+
+QT       += core gui
+QT       +=sql
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = start
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        login.cpp
+
+HEADERS  += login.h
+
+FORMS    += login.ui
